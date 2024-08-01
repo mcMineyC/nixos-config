b1e1be70d7a0ed3b281fae7201bdc5f5d71de476
mcMineyC <jedi.master@falconhosting.net>
2024-08-01 12:23:43 -0700

    Update on Thu Aug  1 12:23:43 PM PDT 2024, gen 8

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -10,7 +10,8 @@
       ./hardware-configuration.nix
       ../../modules/features/coding/coding.nix
       ../../modules/features/games.nix
-      ../../modules/features/hyprland.nix
+      ../../modules/desktops/hyprland/hyprland.nix
+      ../../modules/desktops/gnome/gnome.nix
       inputs.home-manager.nixosModules.default
     ];
 
@@ -43,19 +44,6 @@
 
   programs.hyprland.enable = true;
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
-
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-
-  # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
