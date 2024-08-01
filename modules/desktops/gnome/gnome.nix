3fbab0b614e73fa3de8d24fbf2e05c1b23ab4a3c
mcMineyC <jedi.master@falconhosting.net>
2024-08-01 13:15:44 -0700

    Update on Thu Aug  1 01:15:44 PM PDT 2024, gen 9

--- modules/desktops/gnome/gnome.nix
+++ modules/desktops/gnome/gnome.nix
@@ -1,7 +1,11 @@
 { pkgs, config, ... }:
 
 {
-  pkgs.orchis-theme = pkgs.orchis-theme.override { tweaks = ["macos" "dark"]; };
+  config.packageOverrides = pkgs: {
+    orchis-theme = pkgs.orchis-theme.override { 
+      tweaks = ["macos" "dark"];
+    };
+  };
   environment.systemPackages = with pkgs; [
     gnomeExtensions.blur-my-shell
     gnomeExtensions.media-controls
