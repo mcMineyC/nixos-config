3119d99664d0237d9511a047a8db39a1fb3ee02e
mcMineyC <jedi.master@falconhosting.net>
2024-08-01 13:21:03 -0700

    Update on Thu Aug  1 01:21:03 PM PDT 2024, gen 9

--- modules/desktops/gnome/gnome.nix
+++ modules/desktops/gnome/gnome.nix
@@ -1,32 +1,30 @@
 { pkgs, config, ... }:
 
 {
-  config.packageOverrides = pkgs: {
-    orchis-theme = pkgs.orchis-theme.override { 
-      tweaks = ["macos" "dark"];
-    };
-  };
-  config.environment.systemPackages = with pkgs; [
+  
+  environment.systemPackages = with pkgs; [
     gnomeExtensions.blur-my-shell
     gnomeExtensions.media-controls
     gnome-tweaks
     gtk-engine-murrine
-    orchis-theme
+    pkgs.orchis-theme.override { 
+      tweaks = ["macos" "dark"];
+    }
   ];
   
   # Enable the X11 windowing system.
-  config.services.xserver.enable = true;
+  services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  config.services.xserver.displayManager.gdm.enable = true;
-  config.services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  config.services.xserver.xkb = {
+  services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-  config.services.flatpak = {
+  services.flatpak = {
     enable = true;
     packages = [
       "org.gnome.Extensions"
