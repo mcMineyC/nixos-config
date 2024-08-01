f071a8c592a2e27a53a7b2e2329f138c68732b86
mcMineyC <jedi.master@falconhosting.net>
2024-08-01 13:17:22 -0700

    Update on Thu Aug  1 01:17:22 PM PDT 2024, gen 9

--- modules/desktops/gnome/gnome.nix
+++ modules/desktops/gnome/gnome.nix
@@ -15,18 +15,18 @@
   ];
   
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+  config.services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  config.services.xserver.displayManager.gdm.enable = true;
+  config.services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
-  services.xserver.xkb = {
+  config.services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
-  services.flatpak = {
+  config.services.flatpak = {
     enable = true;
     packages = [
       "org.gnome.Extensions"
