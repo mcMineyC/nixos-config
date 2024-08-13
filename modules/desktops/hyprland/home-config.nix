b79bd45930072d65a8c712968bcb49f27a5bf9fb
mcMineyC <jedi.master@falconhosting.net>
2024-08-13 11:48:37 -0700

    Update on Tue Aug 13 11:48:37 AM PDT 2024, gen 14

--- modules/desktops/hyprland/home-config.nix
+++ modules/desktops/hyprland/home-config.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      monitor = "eDP-1,1920x1080@60,0x0,0";
+      monitor = "eDP-1,1920x1080@60,0x0,0.5";
       "$mod" = "SUPER";
       bind =
         [
