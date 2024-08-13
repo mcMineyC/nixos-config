4341d1b7616a9788c7d01db8f4f222b1a4a8bc7a
mcMineyC <jedi.master@falconhosting.net>
2024-08-13 11:42:59 -0700

    update manually

--- modules/desktops/hyprland/home-config.nix
+++ modules/desktops/hyprland/home-config.nix
@@ -4,7 +4,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     settings = {
-      monitor = ",preferred,auto,1";
+      monitor = "eDP-1,1920x1080@60,0x0,1";
       "$mod" = "SUPER";
       bind =
         [
