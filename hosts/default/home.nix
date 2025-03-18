c162c0b04f6c6d11d86ce7d75eb2ac39ef7c9b9b
mcMineyC <75390386+mcMineyC@users.noreply.github.com>
2025-03-18 11:03:31 -0700

    Update home.nix

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -9,31 +9,35 @@
   ];
 
   colorScheme = inputs.nix-colors.colorSchemes.gruvbox-dark-medium;
+  let
+    username = import ./vars.nix { inherit uname confPath; };
+  in {
+    home.username = uname;
+    home.homeDirectory = "/home/${uname}";
 
-  home.username = "jedi";
-  home.homeDirectory = "/home/jedi";
-  home.stateVersion = "23.11"; # Please read the comment before changing.
-  home.packages = with pkgs; [
-    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
+    home.stateVersion = "23.11"; # Please read the comment before changing.
+    home.packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
 
-    (writeShellScriptBin "sys-rebuild" ''
-      #!/bin/bash
-      if [ "$(pwd)" != "/home/jedi/Documents/nixos-config" ]; then
-        cd /home/jedi/Documents/nixos-config
-        echo "CDed to config dir"
-      fi
-      currentGen=$(nix-env --list-generations | grep current | awk '{print $1}')
-      echo "Now on generation $((currentGen + 1))"
-      echo "Committing changes"
-      git add .
-      git commit -m "Update on $(date), gen $((currentGen + 1))"
-      git push
-      echo "Done"
-      sudo nixos-rebuild switch --flake /home/jedi/Documents/nixos-config#default
-    '')
+      (writeShellScriptBin "sys-rebuild" ''
+        #!/bin/bash
+        if [ "$(pwd)" != "/home/jedi/Documents/nixos-config" ]; then
+          cd /home/jedi/Documents/nixos-config
+          echo "CDed to config dir"
+        fi
+        currentGen=$(nix-env --list-generations | grep current | awk '{print $1}')
+        echo "Now on generation $((currentGen + 1))"
+        echo "Committing changes"
+        git add .
+        git commit -m "Update on $(date), gen $((currentGen + 1))"
+        git push
+        echo "Done"
+        sudo nixos-rebuild switch --flake /home/jedi/Documents/nixos-config#default
+      '')
 
-    spotube
-  ];
+      spotube
+    ];
+  };
 
   home.file = {
     ".config/nvim" = {
