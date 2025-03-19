ae8ce52e0b281292a42e8c7399d755974d993e57
mcMineyC <jedi.master@falconhosting.net>
2025-03-18 21:58:28 -0700

    stuff

--- flake.nix
+++ flake.nix
@@ -14,6 +14,10 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak";
 
     nvim-config.url = "github:mcMineyC/nvim-config";
+    
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
+
   };
 
   outputs = { self, nixpkgs, ... }@inputs: {
---
+++ modules/features/spotify.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, inputs, ... }:
+
+{
+  imports = [
+    inputs.spicetify-nix.homeManagerModules.default
+  ];
+  
+  programs.spicetify =
+  let
+    spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+  in
+  {
+    enable = true;
+    theme = spicePkgs.themes.catppuccin;
+  };
+}
