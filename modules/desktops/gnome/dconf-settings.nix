3a890eca03e1ede3aee46b7f9fc7977dc3c814c6
mcMineyC <jedi.master@falconhosting.net>
2025-04-06 18:17:24 -0700

    Update on Sun Apr  6 06:17:24 PM PDT 2025, gen 3

--- modules/desktops/gnome/dconf-settings.nix
+++ modules/desktops/gnome/dconf-settings.nix
@@ -1,9 +1,34 @@
 { lib, ... }:
 
+with lib.hm.gvariant;
+
 {
   dconf.settings = {
+    # workspace keybinds
     "org/gnome/desktop/wm/keybindings" = {
-
-    }
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-5 = [ "<Super>5" ];
+      switch-to-workspace-6 = [ "<Super>6" ];
+      switch-to-workspace-7 = [ "<Super>7" ];
+      switch-to-workspace-8 = [ "<Super>8" ];
+      switch-to-workspace-9 = [ "<Super>9" ];
+      switch-to-workspace-10 = [ "<Super>0" ];
+    };
+    # apparently gnome automatically wants to open pinned items on dock
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [];
+      switch-to-application-2 = [];
+      switch-to-application-3 = [];
+      switch-to-application-4 = [];
+      switch-to-application-5 = [];
+      switch-to-application-6 = [];
+      switch-to-application-7 = [];
+      switch-to-application-8 = [];
+      switch-to-application-9 = [];
+      switch-to-application-10 = [];
+    };
   };
 }
