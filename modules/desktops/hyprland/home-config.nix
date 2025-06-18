888683f0e643668ac913081a6131b2cb97ef1e46
mcMineyC <jedi.master@falconhosting.net>
2025-06-17 17:58:30 -0700

    Update on Tue Jun 17 05:58:30 PM PDT 2025, gen 5

--- modules/desktops/hyprland/home-config.nix
+++ modules/desktops/hyprland/home-config.nix
@@ -73,30 +73,33 @@
         "$lock_cmd" = pidof hyprlock || hyprlock;
         "$suspend_cmd" = systemctl suspend || loginctl suspend;
 
-        general {
+        general = {
             lock_cmd = "$lock_cmd";
             before_sleep_cmd = loginctl lock-session;
         };
-
-        listener {
+        listener = [
+          {
             timeout = 300; # 5mins
             on-timeout = loginctl lock-session;
-        };
-
-        listener {
+          }
+          {
             timeout = 600; # 10mins
             on-timeout = hyprctl dispatch dpms off;
             on-resume = hyprctl dispatch dpms on;
-        };
-
-        listener {
+          }
+          {
             timeout = 900; # 15mins
             on-timeout = "$suspend_cmd";
-        }
+          }
+
+        ];
       }
     };
     hyprlock = {
       enable = true;
+      settings = {
+        
+      };
     }
   }
 }
